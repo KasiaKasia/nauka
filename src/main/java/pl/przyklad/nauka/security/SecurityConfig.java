159ac9f839aaf4e8c3a0c88b3c67192b013c087f
--add <bulicka.alexandra@gmail.com>
2025-08-07 22:08:03 +0200

    [feat] - Add in-memory user authentication with BCrypt password encoding

--- src/main/java/pl/przyklad/nauka/security/SecurityConfig.java
+++ src/main/java/pl/przyklad/nauka/security/SecurityConfig.java
@@ -4,10 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+import pl.przyklad.nauka.uczen.Uczen;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 /*
  * @EnableWebSecurity - adnotacja informuje Spring , że ta klasa jest odpowiedzialna za konfigurację mechanizmów 
  * zabezpieczeń webowych
@@ -29,11 +37,36 @@ public class SecurityConfig {
 // standardowa nazwa użytkownika to: user, a hasło wyswietla się w konsoli
 
 		// httpBasic - każde request ma być uwierzytelniane przez HTTP basic
-		// dla endpoint "http://localhost:8081/bezUwierzytelniania", "http://localhost:8081/bezHaslaINazwyUzytkownika" będzie wyłaczona autoryzacja
-		http.authorizeHttpRequests(
-				auth -> auth.requestMatchers("/bezUwierzytelniania", "/bezHaslaINazwyUzytkownika").permitAll().anyRequest().authenticated())
-				.httpBasic(withDefaults());
+		// dla endpoint "http://localhost:8081/bezUwierzytelniania",
+		// "http://localhost:8081/bezHaslaINazwyUzytkownika" będzie wyłaczona
+		// autoryzacja
+		http.authorizeHttpRequests(auth -> auth.requestMatchers("/bezUwierzytelniania", "/bezHaslaINazwyUzytkownika")
+				.permitAll().anyRequest().authenticated()).httpBasic(withDefaults());
 
 		return http.build();
 	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+        var root = User.builder()
+                .username("root")
+                .password(passwordEncoder().encode("password"))
+                .roles("ADMIN")
+                .build();
+
+        var janek = User.builder()
+                .username("janek")
+                .password(passwordEncoder().encode("pass"))
+                .roles("CUSTOMER")
+                .build();
+
+
+		return new InMemoryUserDetailsManager(root, janek);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 }
